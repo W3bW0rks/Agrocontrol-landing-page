290c27993327bbbbd8e0c3a229ac0296308a0fda
anonimat0 <andrealonsoarroyo@gmail.com>
2024-09-05 02:42:10 -0500

    feat: update html and css

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navLinks = document.getElementById('nav-links');
-
-    menuToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('visible');
-        navLinks.classList.toggle('hidden');
-    });
-});
